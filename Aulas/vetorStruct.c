cb4b42d8aece531f32031630fb5cca0884290f95
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-08-05 08:40:06 -0300

    Create vetorStruct.c

---
+++ Aulas/vetorStruct.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+struct tipoAluno {
+	char matricula[11];
+	char nome [30];
+	int anoIngresso;
+	float mediaVestibular;
+};
+int main (){
+	struct tipoAluno alunos[2];
+	int i;
+	
+	for(i=0; i<2; i++){
+		printf ("Digite os dados do aluno %d:\n", i+1);
+		
+		printf ("Matricula: ");
+		scanf ("%s", alunos[i].matricula);
+		
+		printf ("Nome do aluno: ");
+		scanf (" %[^\n]", alunos[i].nome);
+		
+		printf ("Ano de ingresso: ");
+		scanf ("%d", &alunos[i].anoIngresso);
+		
+		printf ("Media do vestibular: ");
+		scanf ("%f", &alunos[i].mediaVestibular);
+		
+		printf ("\n");
+	}
+	
+	printf ("\nDADOS DOS ALUNOS\n");
+	for(i=0;i<5; i++){
+		printf ("Aluno %d: \n", i+1);
+		printf ("Matricula: %s\n", alunos[i].matricula);
+		printf ("Nome: %s\n", alunos[i].nome);
+		printf ("Ano de Ingresso: %d\n", alunos[i].anoIngresso);
+		printf ("Media do Vestibular: %.2f\n\n", alunos[i].mediaVestibular);
+	}
+	
+	return 0;
+}
