d629a763b5e8a964cefceb7512d35cda26f87e1f
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-08-12 09:03:16 -0300

    Create Typedef.c

---
+++ Aulas/Typedef.c
@@ -0,0 +1,12 @@
+//TYPEDEF
+
+//declaração do tipo de dado:
+typedef struct {
+	char matricula[11];
+	char nome[30];
+	int anoingresso;
+	float mediavestibular;
+}; taluno;
+
+//declaração da variável do tipo de dado criado:
+taluno aluno;
