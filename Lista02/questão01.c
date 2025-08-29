2ca48c0e9bbb48b9212da61dad8724fd57ccc381
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-08-29 16:32:45 -0300

    Update questão01.c

--- Lista02/questão01.c
+++ Lista02/questão01.c
@@ -1,1 +1,23 @@
-//funções
+#include <stdio.h>
+int conversao(int dia, int mes, int ano){
+	int total;
+	total = (ano * 365) + (mes * 30) + dia;
+	return total;
+}
+int main(){
+	int dias, meses, anos, Total;
+	
+	printf("Digite sua idade em anos, meses e dias: \n");
+	printf("Anos: ");
+	scanf(" %d", &anos);
+	printf("Meses: ");
+	scanf(" %d", &meses);
+	printf("Dias: ");
+	scanf(" %d", &dias);
+	
+	total = conversao(anos, meses, dias);
+	
+	printf("Idade em dias: %d\n", total);
+	
+	return 0;
+}//funções
