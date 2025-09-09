ac2533b23ba2834e26ab55ca84aec9a3fd32814f
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-09-09 08:00:41 -0300

    Update Questão01.c

--- Produtos_Aprendizagem/Produto_01/Questão01.c
+++ Produtos_Aprendizagem/Produto_01/Questão01.c
@@ -1,2 +1,10 @@
-\\1.a
-  struct cliente{
+//questão 01 - a
+struct cliente{
+	char cpf[15];
+	char telefone[15];
+	char nome[50];
+	char data[11];
+	int novo;
+};
+//declaração da variável
+struct cliente c[100];
