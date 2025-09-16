ab7aed075cfacc756746d4482c78b6bc5ca9f64c
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-09-16 08:25:36 -0300

    Update questão01.c

--- Lista04/questão01.c
+++ Lista04/questão01.c
@@ -1,1 +1,21 @@
-teste
+//questão 01 - lista 4
+int somainteiros(int n){
+	if (n==0){
+		return 0;
+	} 
+	else if (n>0){
+		return n + somainteiros(n-1);
+	}
+}
+int main(){
+	int numero, resultado;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &numero);
+	
+	resultado = somainteiros(numero);
+	
+	printf("A soma dos %d primeiros numeros inteiros = %d\n", numero, resultado);
+	
+	return 0;
+}
