37337a3805c80b05794112080571d2664b95eadd
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-09-16 08:43:59 -0300

    Delete Produtos_Aprendizagem/Produto_01 directory

--- Produtos_Aprendizagem/Produto_01/Questão01B.c
+++
@@ -1,8 +0,0 @@
-//questão 01 - b
-//mostrar somente os clientes novos
-int i;
-for(i=0;i<100;i++){
-	if(c[i].novo == 1){
-		printf("%s", c[i].nome);
-	}
-}
--- Produtos_Aprendizagem/Produto_01/Questão01C.c
+++
@@ -1,9 +0,0 @@
-//questão 01 - c 
-//calcular a média e mostrar (valor da ultima compra)
-float s = 0, media;
-for(i=0;i<100;i++){
-	s = s + c[i].valor;
-	//ou s+= c[i].valor
-}
-media = s/100;
-printf ("Media de compras = %f\n", media);
