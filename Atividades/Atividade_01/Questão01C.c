b6381ee544e2e65868ed1ccf298e292f486b5356
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-09-16 08:40:56 -0300

    Update Questão01C.c

--- Atividades/Atividade_01/Questão01C.c
+++ Atividades/Atividade_01/Questão01C.c
@@ -1,8 +1,9 @@
-//questão 01 - b
-//mostrar somente os clientes novos
-int i;
+//questão 01 - c 
+//calcular a média e mostrar (valor da ultima compra)
+float s = 0, media;
 for(i=0;i<100;i++){
-	if(c[i].novo == 1){
-		printf("%s", c[i].nome);
-	}
+	s = s + c[i].valor;
+	//ou s+= c[i].valor
 }
+media = s/100;
+printf ("Media de compras = %f\n", media);
