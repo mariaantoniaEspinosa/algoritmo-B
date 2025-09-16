bc42aac5e5649528d44a64661ce85559ef740dc7
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-09-16 08:03:53 -0300

    Create Recursividade(Potência).c

---
+++ Aulas/Recursividade(Potência).c
@@ -0,0 +1,25 @@
+//Recursividade - Cálculo da Potência
+#include <stdio.h>
+int potencia(int x, int y){
+	if (y==0){
+		return 1;
+	} 
+	else if (y>0){
+		return x * potencia(x, y-1);
+	}
+}
+int main(){
+	int base, expoente, resultado;
+	
+	printf("Digite o valor da base: ");
+	scanf("%d", &base);
+	
+	printf("Digite o valor do expoente: ");
+	scanf("%d", &expoente);
+	
+	resultado = potencia(base, expoente);
+	
+	printf("%d^%d = %d\n", base, expoente, resultado);
+	
+	return 0;
+}
