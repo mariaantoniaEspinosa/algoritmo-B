40f1f986c8e0dbeb9e5ae775fcc9a751bdd1ed75
Maria Antônia Espinosa <m.antonia.e.w@gmail.com>
2025-11-08 20:09:28 -0300

    Update Questão01.c

--- Lista06/Questão01.c
+++ Lista06/Questão01.c
@@ -1,1 +1,18 @@
-escrever aqui
+//lista 6 questão 01
+#include <stdio.h>
+void vertical(char *str){
+	while(*str != '\0'){
+		printf("%c\n", *str);
+		str++;
+	}
+}
+int main(){
+	char s[100];
+	
+	printf("Digite uma palavra: ");
+	gets(s);
+	
+	vertical(s);
+	
+	return 0;
+}
